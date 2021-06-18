8963241bd01b70094a6d98c418644bac571df19f
Eduardo Jesus <edu.ribeiro.jesus@gmail.com>
2021-06-18 14:56:39 -0300

    Atualização no código

--- Atividade05/src/Questao02.java
+++ Atividade05/src/Questao02.java
@@ -3,13 +3,15 @@ import javax.swing.JOptionPane;
 public class Questao02 {
 
 	public static void main(String[] args) {
-		Object[] lsOpcoes = new Object[] { "Pedra", "Papel", "Tesoura" };
-		Object[] confirmar = new Object[] {"Sim", "Não"};
+		Object[] lsOpcoes = { "Pedra", "Papel", "Tesoura" };
+		
 		
 		int jogarNovamente = 0;
 		int vitoria = 0;
 		int empate = 0;
 		int derrota = 0;
+		
+		Object[] continuar = {"Sim", "Não"};
 		
 		while (jogarNovamente == 0) {
 			int escolherOpcao = JOptionPane.showOptionDialog(null, "Escolha uma opção!", "Escolha!", 0, JOptionPane.INFORMATION_MESSAGE, null, lsOpcoes, lsOpcoes[0]);
@@ -20,16 +22,16 @@ public class Questao02 {
 				mensagem = "Empate";
 				++empate;			
 			}
-			else if ((escolherOpcao != 0 || maquina != 2) && (escolherOpcao != 1 || maquina != 0) && (escolherOpcao != 2 || maquina != 1)){
-				mensagem = "Derrota";
-				++derrota;				
+			else if ((escolherOpcao == 0 && maquina == 2) || (escolherOpcao == 1 && maquina == 0) || (escolherOpcao == 2 && maquina == 1)){
+				mensagem = "Vitoria";
+				++vitoria;				
 			}
 			else {
-				mensagem = "Vitória";
-				++vitoria;
+				mensagem = "Derrota";
+				++derrota;
 			}
 			
-			jogarNovamente = JOptionPane.showOptionDialog(null, "Eu (" +lsOpcoes[escolherOpcao]+") X Máquina ("+lsOpcoes[maquina]+")\nResultado: "+ mensagem+ "\nJogar Novamente?", "Jogo", 0, 3, null, confirmar, confirmar[0]);
+			jogarNovamente = JOptionPane.showOptionDialog(null, "Eu (" +lsOpcoes[escolherOpcao]+") X Máquina ("+lsOpcoes[maquina]+")\nResultado: "+ mensagem+ "\nJogar Novamente?", "Jogo", 0, JOptionPane.INFORMATION_MESSAGE, null, continuar, continuar[0]);
 		}
 		
 		JOptionPane.showMessageDialog(null, "Placar final\nVitórias: " +vitoria+ 
