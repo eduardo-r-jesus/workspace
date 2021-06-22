6dac700e4ff1e29080c7c7afa95dc02f53f4adb6
Eduardo Jesus <edu.ribeiro.jesus@gmail.com>
2021-06-22 19:21:49 -0300

    Commit - Atualização do código.

--- Atividade06/src/Main.java
+++ Atividade06/src/Main.java
@@ -1,10 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import javax.swing.JOptionPane;
 
-public class Main extends Pessoa {
+public class Main {
 
 	public static void main(String[] args) {
 
 		Pessoa p = new Pessoa();
+		DateTimeFormatter formatarData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 		boolean aux = false;
 
@@ -19,7 +23,7 @@ public class Main extends Pessoa {
 
 			} catch (Exception e) {
 				{
-
+					JOptionPane.showMessageDialog(null, "Favor, informe o seu nome!");
 				}
 			}
 		}
@@ -27,60 +31,28 @@ public class Main extends Pessoa {
 		aux = false;
 		while (!aux) {
 			try {
-				p.setDiaNascimento(Integer.parseInt(JOptionPane.showInputDialog("Informe o DIA do seu nascimento: ")));
-				if (p.getDiaNasc() < 1 || p.getDiaNasc() > 31) {
-					JOptionPane.showMessageDialog(null, "O DIA informado não é válido!");
-				} else {
-					aux = true;
-				}
-
+				p.setDatNasc(
+						LocalDate.parse(
+								JOptionPane.showInputDialog(
+										"Informe sua data de nascimento informando no formato DD/MM/AAAA: "),
+								formatarData));
+				break;
 			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "Favor, informe o DIA do seu nascimento somente com números!");
+				JOptionPane.showMessageDialog(null, "Favor, informe sua data de nascimento no formato DD/MM/AAAA!");
 			}
 		}
 
 		aux = false;
 		while (!aux) {
 			try {
-				p.setMesNascimento(Integer.parseInt(JOptionPane.showInputDialog("Informe o MÊS do seu nascimento: ")));
-				if (p.getMesNasc() < 1 || p.getMesNasc() > 12) {
-					JOptionPane.showMessageDialog(null, "O MÊS informado não é válido!");
-				} else {
-					aux = true;
-				}
-			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "Favor, informe o MÊS do seu nascimento!");
-			}
-		}
-
-		aux = false;
-		while (!aux) {
-			try {
-				p.setAnoNascimento(Integer.parseInt(JOptionPane.showInputDialog("Informe o ANO do seu nascimento: ")));
-				if (p.getAnoNasc() < 1900 || p.getAnoNasc() > p.hoje.getYear()) {
-					JOptionPane.showMessageDialog(null, "O ANO informado não é válido!");
-				} else {
-					aux = true;
-				}
-			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "Favor, informe o ANO do seu nascimento!");
-			}
-		}
-
-		aux = false;
-		while (!aux)
-		{
-			try {
-				p.setAltura(Float.parseFloat(JOptionPane.showInputDialog("Informe sua altura: ")));
+				p.setAltura(Float.parseFloat(JOptionPane.showInputDialog("Informe sua altura: ").replace(",", ".")));
 				break;
 			} catch (Exception e) {
 				JOptionPane.showMessageDialog(null, "Informe somente números e/ou . ao inevez de , !");
+				e.printStackTrace();
 			}
-
 		}
-
-		p.exibirDadosPessoa();
-
+		JOptionPane.showMessageDialog(null, p.exibirDadosPessoa());
 	}
 
 }
--- Atividade06/src/Pessoa.java
+++ Atividade06/src/Pessoa.java
@@ -1,14 +1,12 @@
 import java.time.LocalDate;
-
-import javax.swing.JOptionPane;
+import java.time.format.DateTimeFormatter;
 
 public class Pessoa {
 
 	private String nome;
-	private int diaNasc;
-	private int mesNasc;
-	private int anoNasc;
 	private float alt;
+	private LocalDate datNasc;
+
 	LocalDate hoje = LocalDate.now();
 
 	public String getNome() {
@@ -19,30 +17,6 @@ public class Pessoa {
 		this.nome = nome;
 	}
 
-	public int getDiaNasc() {
-		return diaNasc;
-	}
-
-	public void setDiaNascimento(int diaNasc) {
-		this.diaNasc = diaNasc;
-	}
-
-	public int getMesNasc() {
-		return mesNasc;
-	}
-
-	public void setMesNascimento(int mesNasc) {
-		this.mesNasc = mesNasc;
-	}
-
-	public int getAnoNasc() {
-		return anoNasc;
-	}
-
-	public void setAnoNascimento(int anoNasc) {
-		this.anoNasc = anoNasc;
-	}
-
 	public float getAltura() {
 		return alt;
 	}
@@ -51,27 +25,27 @@ public class Pessoa {
 		this.alt = altura;
 	}
 
-	public void exibirDadosPessoa() {
-		JOptionPane.showMessageDialog(null,
-				"Seu nome é: " + getNome() + "\nSua data de nascimento é: " + getDiaNasc() + "/" + getMesNasc() + "/"
-						+ getAnoNasc() + "\nSua Idade é: " + calculaIdade() + "\nSua altura é: " + getAltura());
+	public LocalDate getDatNasc() {
+		return datNasc;
+	}
+
+	public void setDatNasc(LocalDate datNasc) {
+		this.datNasc = datNasc;
+	}
+
+	public String exibirDadosPessoa() {
+		DateTimeFormatter formatarData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		return "Seu nome é: " + getNome() + "\nSua Data de Nascimento: " + getDatNasc().format(formatarData)
+				+ "\nSua Idade é: " + calculaIdade() + "\nSua altura é: " + getAltura();
 	}
 
 	public int calculaIdade() {
-		
-		int diah = hoje.getDayOfMonth();
-		int mesh = hoje.getMonthValue() + 1;
-		int anoh = hoje.getYear();
-
-		int idade;
-
-		if (mesNasc < mesh || (mesNasc == mesh && diaNasc <= diah))
-			idade = anoh - anoNasc;
-		else
-			idade = (anoh - anoNasc) - 1;
-
-		return (idade);
-
+		int idade = (hoje.getYear() - getDatNasc().getYear());
+		if (getDatNasc().getMonthValue() > hoje.getMonthValue() || (getDatNasc().getMonthValue() == hoje.getMonthValue()
+				&& getDatNasc().getDayOfMonth() > hoje.getDayOfMonth())) {
+			idade--;
+		}
+		return idade;
 	}
 
 }
